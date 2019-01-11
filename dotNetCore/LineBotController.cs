f086d55b2e4d0e56a5cd3998a8929ecabf3fb07b
twdeveloper@vmwinsrv2016 <twDeveloper@hotmail.com>
2019-01-11 22:54:01 +0800

    v1

---
+++ dotNetCore/LineBotController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dotNetCore
+{
+    [Route("api/LineBot")]
+    public class LineBotController : Controller
+    {
+        [HttpPost]
+        public StatusCodeResult OnPost()
+        {
+            var ReceivedMsg = isRock.LineBot.Bot.ParsingReceivedMessage(Request.Body);
+            if (ReceivedMsg.events[0].replyToken == "00000000000000000000000000000000")
+                return Ok();
+
+            //var touserId = "";
+            var token = "";
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot(token);
+
+            isRock.LineBot.TextMessage msg;
+            if (ReceivedMsg.events.Count > 0 && ReceivedMsg.events[0].message != null && !string.IsNullOrEmpty(ReceivedMsg.events[0].message.text))
+            {
+
+                msg = new isRock.LineBot.TextMessage("you said : " + ReceivedMsg.events[0].message.text);
+            }
+            else
+            {
+                msg = new isRock.LineBot.TextMessage("test");
+            }
+            bot.ReplyMessage(ReceivedMsg.events[0].replyToken, msg);
+
+            return Ok();
+        }
+    }
+}
---
+++ dotNetCore/Pages/PushMessage.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotNetCore.Pages
+{
+    public class PushMessageModel : PageModel
+    {
+        //請輸入你的 Channel Access Token
+        string ChannelAccessToken = ""; // "~~~~~請輸入你的請輸入你的 Channel Access Token~~~~~~~";
+        //請輸入你的User Id
+        string AdminUserId = ""; //"~~~~~請輸入你的User Id~~~~~~~";
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            var bot = new isRock.LineBot.Bot(ChannelAccessToken);
+            var msg = new isRock.LineBot.TextMessage("Hello World");
+            bot.PushMessage(AdminUserId, msg);
+
+            return Page();
+        }
+    }
+}
